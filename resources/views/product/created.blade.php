2b372228ee1145fe5c1c7a8e79a7a01c69a11238
ACarmona4 <acarmonas@eafit.edu.co>
2025-08-11 13:01:49 -0500

    Tutorial 1

---
+++ resources/views/product/created.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Product Created')
+@section('subtitle', 'Confirmation')
+
+@section('content')
+<div class="alert alert-success mt-4" role="alert">
+    Product created
+</div>
+@endsection
