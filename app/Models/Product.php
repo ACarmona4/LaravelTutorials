b8afcc33250d8e8cd3859fde3ae244adf42f91f9
ACarmona4 <acarmonas@eafit.edu.co>
2025-08-22 11:13:02 -0500

    Tutorial 3

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,14 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    use HasFactory;
-
     /**
      * PRODUCT ATTRIBUTES
      *
