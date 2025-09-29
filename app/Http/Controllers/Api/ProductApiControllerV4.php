77be57d49981a200470eca95ae1d6637e0bdcf4f
ACarmona4 <acarmonas@eafit.edu.co>
2025-09-28 19:25:52 -0500

    Tutorial API

---
+++ app/Http/Controllers/Api/ProductApiControllerV4.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class ProductApiControllerV4 extends Controller
+{
+    public function store(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric|gt:0',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json([
+            'message' => 'Product created',
+            'product' => $product,
+        ], 201);
+    }
+}
